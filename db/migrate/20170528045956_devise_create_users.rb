b72dc73c4f20ff66cd9d6e9b4f1f872264789816
Chris Hahn <chrishahnd@gmail.com>
2017-06-06 06:13:15 +0000

    Lessons permissions

--- db/migrate/20170528045956_devise_create_users.rb
+++ db/migrate/20170528045956_devise_create_users.rb
@@ -1,5 +1,5 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.0]
-  before_action :authenticate_user!
+before_action :authenticate_user!
   def change
     create_table :users do |t|
       ## Database authenticatable
